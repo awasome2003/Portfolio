295fad326447b25be36309510cac18fdb19ca808
awasome2003 <147895180+awasome2003@users.noreply.github.com>
2025-04-24 12:26:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), visualizer({ open: true })],
+  base: "/Portfolio/",
 });
